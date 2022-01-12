3ef20652d887f90dccfa8fc1d46af832cb901085
Jacqueline Chong <89925896+Jacq4nn@users.noreply.github.com>
2022-01-12 12:29:11 -0800

    Added docstring for function perc_cap_words

---
+++ src/textfeatureinfo/textfeatureinfo.py
@@ -0,0 +1,39 @@
+# Count number of punctuations
+
+
+
+
+
+
+
+
+
+# Average count length
+
+
+
+
+
+
+
+
+
+# Count percentage of fully capitalised words
+def perc_cap_words(text):
+    """
+    Calculate percentage of fully capitalised words in the text.
+   
+    Parameters:
+    ------
+    text: (str)
+    the input text
+    
+    Returns:
+    -------
+    percentage of capitalised words: float
+    """
+
+
+
+
+# Remove stopwords
