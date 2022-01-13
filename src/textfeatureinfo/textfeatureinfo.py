33cc3906f6861dfc8534db30bfa4b062969b20cb
Kiran Phaterpekar <kphaterp@student.ubc.ca>
2022-01-12 19:27:09 -0800

    Add documentation to average word length function

---
+++ src/textfeatureinfo/textfeatureinfo.py
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+
+
+
+
+def avg_word_len(text):
+    """
+    Calculates the average length of the words within the text.
+
+    Parameters:
+    ___________
+    text: (str)
+    piece of text to analyze
+
+    Returns
+    _______
+    the average length of words within the text
+
+    Examples
+    ________
+    >>> avg_word_len("Hello, World!")
+    """
